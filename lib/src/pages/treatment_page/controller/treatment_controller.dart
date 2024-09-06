08b31eb85f7dc1577ac983a93f1e7c15cb1cd937
welldone <amirvaladan1993@gmail.com>
2024-09-06 21:32:43 +0330

    create treatment timer

--- lib/src/pages/treatment_page/controller/treatment_controller.dart
+++ lib/src/pages/treatment_page/controller/treatment_controller.dart
@@ -1,7 +1,67 @@
+import 'dart:async';
+
 import 'package:get/get.dart';
 import 'package:hemo_dialysis/src/infrastructure/routes/route_names.dart';
 
 class TreatmentPageController extends GetxController {
+  Timer? timer;
+
+  final RxBool isTreatmentDone = false.obs;
+
+  final RxString remainingSecond = '00'.obs,
+      remainingMinute = '30'.obs,
+      remainingHour = '04'.obs;
+
+  final RxInt secondsLeft = (11).obs;
+
+  // final RxInt secondsLeft = (16200).obs;
+
+  @override
+  void onInit() {
+    _timer();
+    super.onInit();
+  }
+
+  void _timer() {
+    timer = Timer.periodic(
+      const Duration(seconds: 1),
+      (timer) {
+        if (secondsLeft.value > 0) {
+          secondsLeft.value -= 1;
+          _formatTime(secondsLeft.value);
+        } else {
+          timer.cancel();
+          isTreatmentDone.value = true;
+        }
+      },
+    );
+  }
+
+  void _formatTime(final int seconds) {
+    int hour = (seconds ~/ 3600);
+    int minute = _minuteExtractor(hour: hour, seconds: seconds);
+    remainingHour.value = _getTimeWithZero(hour);
+    remainingMinute.value = _getTimeWithZero(minute);
+    int second = seconds % 60;
+    remainingSecond.value = _getTimeWithZero(second);
+  }
+
+  int _minuteExtractor({
+    required final int hour,
+    required final int seconds,
+  }) =>
+      (hour == 0) ? seconds ~/ 60 : (seconds - (hour * 3600)) ~/ 60;
+
+  String _getTimeWithZero(final int minute) {
+    if (minute == 0) {
+      return '00';
+    } else if (minute < 10) {
+      return '0$minute';
+    } else {
+      return minute.toString();
+    }
+  }
+
   void navigateToBloodReturnPage() =>
       Get.toNamed(HemoDialysisRouteNames.bloodReturnPage.uri);
 }
--- lib/src/pages/treatment_page/view/treatment_page_view.dart
+++ lib/src/pages/treatment_page/view/treatment_page_view.dart
@@ -1,8 +1,10 @@
+import 'package:auto_size_text/auto_size_text.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
 import '../../../components/button_with_text.dart';
 import '../../../components/scaffold.dart';
+import '../../../infrastructure/utils/constants.dart';
 import '../../../infrastructure/utils/decorations.dart';
 import '../../shared/view/app_bar_back_icon.dart';
 import '../../shared/view/app_bar_support_icon.dart';
@@ -13,12 +15,131 @@ class TreatmentPageView extends GetView<TreatmentPageController> {
 
   @override
   Widget build(BuildContext context) => CustomScaffold(
-        body: _body(),
+        body: Obx(_body),
         action1: const AppBarBackIcon(),
         action2: const AppBarSupportIcon(),
       );
 
-  Widget _body() => FractionallySizedBox(
+  Widget _body() =>
+      controller.isTreatmentDone.value ? _afterTreatment() : _beforeTreatment();
+
+  Widget _beforeTreatment() => FractionallySizedBox(
+        heightFactor: 0.9,
+        widthFactor: 0.9,
+        child: Column(
+          children: [
+            Expanded(child: _treatmentTimer()),
+            Constants.giantVerticalSpacer,
+            Expanded(child: _treatmentInfo())
+          ],
+        ),
+      );
+
+  Widget _treatmentInfo() => DecoratedBox(
+        decoration: Decorations.cardDecoration(),
+        child: Placeholder(),
+      );
+
+  Widget _treatmentTimer() => DecoratedBox(
+        decoration: Decorations.cardDecoration(),
+        child: FractionallySizedBox(
+          heightFactor: 0.8,
+          child: Row(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Expanded(child: Placeholder()),
+              Expanded(
+                flex: 4,
+                child: FractionallySizedBox(
+                  widthFactor: 0.8,
+                  child: Column(
+                    children: [
+                      _actualTime(),
+                      Expanded(
+                        child: Obx(
+                          () => Row(
+                            children: [
+                              Expanded(
+                                child: _timeCounter(
+                                  title: 'ثانیه',
+                                  count: controller.remainingSecond.value,
+                                ),
+                              ),
+                              Expanded(
+                                child: _timeCounter(
+                                  title: 'دقیقه',
+                                  count: controller.remainingMinute.value,
+                                ),
+                              ),
+                              Expanded(
+                                child: _timeCounter(
+                                  title: 'ساعت',
+                                  count: controller.remainingHour.value,
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+              ),
+              Expanded(flex: 4, child: _hintText()),
+            ],
+          ),
+        ),
+      );
+
+  Widget _timeCounter({
+    required final String count,
+    required final String title,
+  }) =>
+      Padding(
+        padding: const EdgeInsets.all(Constants.largeSpace),
+        child: Column(
+          children: [
+            Expanded(
+              child: DecoratedBox(
+                decoration: BoxDecoration(
+                  border: Border.all(),
+                  borderRadius: const BorderRadius.all(
+                    Radius.circular(12),
+                  ),
+                ),
+                child: Center(
+                  child: AutoSizeText(
+                    count,
+                    style: const TextStyle(
+                      fontFamily: Constants.iranSansFaNumFont,
+                      fontWeight: FontWeight.w500,
+                      fontSize: 35,
+                    ),
+                  ),
+                ),
+              ),
+            ),
+            Constants.mediumVerticalSpacer,
+            AutoSizeText(
+              title,
+              style: const TextStyle(
+                fontFamily: Constants.iranSansFaNumFont,
+                fontWeight: FontWeight.w500,
+              ),
+            ),
+          ],
+        ),
+      );
+
+  Widget _actualTime() => const AutoSizeText(
+        'زمان باقیمانده از 4 ساعت و 30 دقیقه',
+        style: TextStyle(
+          fontFamily: Constants.iranSansFaNumFont,
+          fontWeight: FontWeight.w500,
+        ),
+      );
+
+  Widget _afterTreatment() => FractionallySizedBox(
         heightFactor: 0.9,
         widthFactor: 0.9,
         child: DecoratedBox(
@@ -43,5 +164,38 @@ class TreatmentPageView extends GetView<TreatmentPageController> {
             action: controller.navigateToBloodReturnPage,
           ),
         ],
+      );
+
+  Widget _hintText() => const FractionallySizedBox(
+        heightFactor: 0.3,
+        widthFactor: 0.6,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.end,
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            Expanded(
+              child: AutoSizeText(
+                'در حال درمان',
+                style: TextStyle(
+                  fontFamily: Constants.iranSansFont,
+                  color: Constants.disableColor,
+                  fontWeight: FontWeight.w500,
+                ),
+                textDirection: TextDirection.rtl,
+              ),
+            ),
+            Expanded(
+              child: AutoSizeText(
+                'همودیالیز در حال انجام است....',
+                style: TextStyle(
+                  fontFamily: Constants.iranSansFont,
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                ),
+                textDirection: TextDirection.rtl,
+              ),
+            ),
+          ],
+        ),
       );
 }
