09815aafac7464a831758d6cc00b051e5e0d5911
a.valadan <amirvaladan1993@gmail.com>
2025-02-08 22:41:27 +0330

    complete treatment calendar page

--- lib/src/pages/profile_page/view/widget/treatment_calendar.dart
+++ lib/src/pages/profile_page/view/widget/treatment_calendar.dart
@@ -20,6 +20,22 @@ class TreatmentCalendar extends GetView<ProfilePageController> {
         ),
       );
 
+  Widget _firstMonth(BuildContext context) => JalaliTableCalendar(
+        context: context,
+        marker: (date, events) => const ColoredBox(
+          color: Colors.red,
+          child: SizedBox(
+            height: 2,
+            width: 15,
+          ),
+        ),
+        events: {
+          controller.today.add(const Duration(days: -3)): const [],
+          controller.today.add(const Duration(days: 1)): const [],
+          controller.today.add(const Duration(days: 5)): const [],
+        },
+      );
+
   Widget _body(BuildContext context) => FractionallySizedBox(
         widthFactor: 0.9,
         child: Column(
@@ -41,25 +57,29 @@ class TreatmentCalendar extends GetView<ProfilePageController> {
                 ],
               ),
             ),
-            const Expanded(child: Placeholder()),
+            Expanded(child: _addDatePart()),
           ],
         ),
       );
 
-  Widget _firstMonth(BuildContext context) => JalaliTableCalendar(
-        context: context,
-        marker: (date, events) => const ColoredBox(
-          color: Colors.red,
-          child: SizedBox(
-            height: 2,
-            width: 15,
+  Widget _addDatePart() => Row(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: [
+          _title('افزودن تاریخ جدید'),
+          Constants.largeHorizontalSpacer,
+          const DecoratedBox(
+            decoration: BoxDecoration(
+                color: Constants.deSelectedChipTextColor,
+                borderRadius: BorderRadius.all(Radius.circular(8))),
+            child: Padding(
+              padding: Constants.smallPadding,
+              child: Icon(
+                Icons.add,
+                color: Constants.whiteColor,
+              ),
+            ),
           ),
-        ),
-        events: {
-          controller.today.add(const Duration(days: -3)): const [],
-          controller.today.add(const Duration(days: 1)): const [],
-          controller.today.add(const Duration(days: 5)): const [],
-        },
+        ],
       );
 
   Widget _secondMonth(BuildContext context) => JalaliTableCalendar(
@@ -83,70 +103,71 @@ class TreatmentCalendar extends GetView<ProfilePageController> {
         children: [_guideBody(), _guideTitle()],
       );
 
-  Widget _guideBody() => DecoratedBox(
-        decoration: BoxDecoration(
-          border: Border.all(
-            color: Constants.whiteColor,
-            width: 0.5,
+  Widget _guideBody() => FractionallySizedBox(
+        heightFactor: 0.9,
+        widthFactor: 0.9,
+        child: DecoratedBox(
+          decoration: BoxDecoration(
+            border: Border.all(
+              color: Constants.whiteColor,
+              width: 0.5,
+            ),
+            borderRadius: const BorderRadius.all(
+              Radius.circular(Constants.largeSpace),
+            ),
           ),
-          borderRadius: const BorderRadius.all(
-            Radius.circular(Constants.largeSpace),
-          ),
-        ),
-        child: Padding(
-          padding: const EdgeInsets.symmetric(
-            horizontal: Constants.largeSpace,
-            vertical: Constants.xxLargeSpace,
-          ),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.end,
-            children: [
-              Expanded(
-                child: DecoratedBox(
-                  decoration: const BoxDecoration(
-                    color: Constants.buttonFourthColor,
-                    borderRadius: BorderRadius.all(
-                      Radius.circular(Constants.largeSpace),
+          child: Padding(
+            padding: Constants.mediumPadding,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                Expanded(
+                  child: DecoratedBox(
+                    decoration: const BoxDecoration(
+                      color: Constants.buttonFourthColor,
+                      borderRadius: BorderRadius.all(
+                        Radius.circular(Constants.largeSpace),
+                      ),
                     ),
+                    child: Center(child: _buttonTitle('آزمایش ماهیانه')),
                   ),
-                  child: Center(child: _buttonTitle('آزمایش ماهیانه')),
                 ),
-              ),
-              Constants.mediumHorizontalSpacer,
-              Expanded(
-                child: DecoratedBox(
-                  decoration: const BoxDecoration(
+                Constants.mediumHorizontalSpacer,
+                Expanded(
+                  child: DecoratedBox(
+                    decoration: const BoxDecoration(
+                      color: Constants.iconThirdColor,
+                      borderRadius: BorderRadius.all(
+                        Radius.circular(Constants.largeSpace),
+                      ),
+                    ),
+                    child: Center(child: _buttonTitle('چکاپ ماهیانه')),
+                  ),
+                ),
+                Constants.mediumHorizontalSpacer,
+                const AutoSizeText(
+                  'امروز',
+                  style: TextStyle(
+                    fontFamily: Constants.iranSansFont,
+                    fontWeight: FontWeight.w300,
+                    fontSize: 14,
                     color: Constants.iconThirdColor,
-                    borderRadius: BorderRadius.all(
-                      Radius.circular(Constants.largeSpace),
-                    ),
                   ),
-                  child: Center(child: _buttonTitle('چکاپ ماهیانه')),
                 ),
-              ),
-              Constants.mediumHorizontalSpacer,
-              const AutoSizeText(
-                'امروز',
-                style: TextStyle(
-                  fontFamily: Constants.iranSansFont,
-                  fontWeight: FontWeight.w300,
-                  fontSize: 14,
-                  color: Constants.iconThirdColor,
+                Constants.mediumHorizontalSpacer,
+                const AutoSizeText(
+                  'همودیالیز',
+                  style: TextStyle(
+                    decoration: TextDecoration.underline,
+                    decorationColor: Constants.whiteColor,
+                    fontFamily: Constants.iranSansFont,
+                    fontWeight: FontWeight.w300,
+                    fontSize: 14,
+                    color: Constants.whiteColor,
+                  ),
                 ),
-              ),
-              Constants.mediumHorizontalSpacer,
-              const AutoSizeText(
-                'همودیالیز',
-                style: TextStyle(
-                  decoration: TextDecoration.underline,
-                  decorationColor: Constants.whiteColor,
-                  fontFamily: Constants.iranSansFont,
-                  fontWeight: FontWeight.w300,
-                  fontSize: 14,
-                  color: Constants.whiteColor,
-                ),
-              ),
-            ],
+              ],
+            ),
           ),
         ),
       );
