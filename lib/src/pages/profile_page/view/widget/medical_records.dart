ba7ec3accd907655ae33fd96ee248da9ca3e422d
a.valadan <amirvaladan1993@gmail.com>
2025-02-08 22:57:21 +0330

    complete medical records page

--- lib/src/pages/profile_page/view/widget/medical_records.dart
+++ lib/src/pages/profile_page/view/widget/medical_records.dart
@@ -1,6 +1,8 @@
+import 'package:auto_size_text/auto_size_text.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
+import '../../../../infrastructure/utils/constants.dart';
 import '../../../../infrastructure/utils/decorations.dart';
 import '../../controller/profile_page_controller.dart';
 
@@ -10,35 +12,110 @@ class MedicalRecords extends GetView<ProfilePageController> {
   @override
   Widget build(BuildContext context) => DecoratedBox(
         decoration: Decorations.secondaryCardDecoration(),
-        child: const FractionallySizedBox(
+        child: FractionallySizedBox(
           heightFactor: 0.8,
           widthFactor: 0.95,
-          child: Placeholder(),
+          child: FractionallySizedBox(
+            widthFactor: 0.9,
+            heightFactor: 0.9,
+            child: Row(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Expanded(child: _addDocPart()),
+                Expanded(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.end,
+                    children: [
+                      Expanded(flex: 2, child: _title('اسناد درمانی کاربر')),
+                      Constants.giantVerticalSpacer,
+                      Expanded(
+                        flex: 5,
+                        child: Column(
+                          children: [
+                            _docsTitle('اسناد مربوط به آزمایشگاه'),
+                            Constants.mediumVerticalSpacer,
+                            _docsTitle('آرشیو نسخه ها'),
+                            Constants.mediumVerticalSpacer,
+                            _docsTitle('اسناد مربوط به تصویر برداری ها'),
+                            Constants.mediumVerticalSpacer,
+                            _docsTitle('اسناد مربوط به رژیم غذایی'),
+                            Constants.mediumVerticalSpacer,
+                            _docsTitle('اسناد دیگر'),
+                          ],
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
         ),
       );
 
-// Widget _body() => Obx(
-//       () => DecoratedBox(
-//         decoration: Decorations.secondaryCardDecoration(),
-//         child: FractionallySizedBox(
-//           heightFactor: 0.95,
-//           widthFactor: 0.95,
-//           child: controller.canShowData.value ? _data() : const SizedBox(),
-//         ),
-//       ),
-//     );
+  Widget _title(String text) => AutoSizeText(
+        text,
+        style: const TextStyle(
+          fontFamily: Constants.iranSansFaNumFont,
+          fontWeight: FontWeight.w500,
+          fontSize: 20,
+          color: Constants.whiteColor,
+        ),
+      );
 
-// Widget _data2() => const Row(
-//       children: [
-//         Expanded(
-//           child: Column(
-//             children: [
-//               Expanded(flex: 3, child: PrescriptionInfo()),
-//               Expanded(flex: 5, child: PatientInfo()),
-//             ],
-//           ),
-//         ),
-//         Expanded(child: PersonalInfo()),
-//       ],
-//     );
+  Widget _docsTitle(String text) => Expanded(
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.end,
+          children: [
+            const Icon(
+              Icons.edit,
+              color: Constants.iconSecondColor,
+            ),
+            Constants.smallHorizontalSpacer,
+            const Icon(
+              Icons.download,
+              color: Constants.iconThirdColor,
+            ),
+            const Spacer(),
+            AutoSizeText(
+              text,
+              style: const TextStyle(
+                fontFamily: Constants.iranSansFaNumFont,
+                fontWeight: FontWeight.w300,
+                fontSize: 20,
+                color: Constants.whiteColor,
+                decoration: TextDecoration.underline,
+                decorationColor: Constants.whiteColor,
+              ),
+            ),
+            Constants.largeHorizontalSpacer,
+            const ColoredBox(
+              color: Constants.buttonSecondaryColor,
+              child: SizedBox(
+                height: 20,
+                width: 5,
+              ),
+            ),
+          ],
+        ),
+      );
+
+  Widget _addDocPart() => Row(
+        children: [
+          _title('افزودن سند جدید'),
+          Constants.largeHorizontalSpacer,
+          const DecoratedBox(
+            decoration: BoxDecoration(
+                color: Constants.deSelectedChipTextColor,
+                borderRadius: BorderRadius.all(Radius.circular(8))),
+            child: Padding(
+              padding: Constants.smallPadding,
+              child: Icon(
+                Icons.add,
+                color: Constants.whiteColor,
+              ),
+            ),
+          ),
+        ],
+      );
 }
